d0660ca262c7f97f4b1c2154fcf9f098160d6325
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-11-30 08:43:38 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,8 +1,8 @@
-provider "aws"{
+provider "aws" {
    region  = "us-east-1" 
 }
 
-resource "aws_instance" "AWSEC2Instance"{
+resource "aws_instance" "AWSEC2Instance" {
      count   = 1
      ami = "ami-05723c3b9cf4bf4ff"
      instance_type = "t2.micro"
