c8ae156ca8a189d3a5ff594f40d89015475dca72
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-12-01 08:47:48 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -13,7 +13,97 @@ resource "aws_instance" "AWSEC2Instance"{
      }
 }
 */
+
+ variable "awsprops" {
+    type = "map"
+    default = {
+    region = "us-east-1"
+    vpc = "vpc-05e24a8891f346a4f"
+    ami = "ami-05723c3b9cf4bf4ff"
+    itype = "t2.micro"
+    subnet = "subnet-00711afa00bb04391"
+    publicip = true
+    keyname = "realkeypair"
+    secgroupname = "Nexus-SG"
+  }
+}  
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+}
+
+resource "aws_security_group" "Nexus-SG" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
    
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+   
+   // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = ""
+    to_port = 80
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+   
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["10.0.0.0/16"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+  
+resource "aws_instance" "AWSEC2Instance" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+ 
+   
+vpc_security_group_ids = [
+    aws_security_group.AWSEC2Instance-Nexus-SG.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    iops = 150
+    volume_size = 10
+    volume_type = "gp2"
+  }
+   
+tags = {
+    Name ="tomcatserver"
+  }
+  
+  depends_on = [ aws_security_group.AWSEC2Instance-Nexus-SG ]
+   
+output "ec2instance" {
+  value = aws_instance.AWSEC2Instance.public_ip
+}
+   
+ }
+
+
+
+
+
+
+
+
+
+/*
 provider "aws" {
   region = "us-east-1"
 }
@@ -65,4 +155,4 @@ resource "aws_instance" "AWSEC2Instance" {
     Name = "tomcatserver"
   }
 }
-
+*/
