5d5fb73da431d7d32e8e28648f360aa337b1c42d
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-12-01 00:19:43 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
    region  = "us-east-1" 
 }
 
@@ -11,5 +11,47 @@ resource "aws_instance" "AWSEC2Instance"{
      tags = {
         Name = "tomcatserver"
      }
+}
+*/
+   
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_security_group" "Nexus-SG" {
+
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+}
+
+
+resource "aws_instance" "AWSEC2Instance" {
+  ami           = "ami-05723c3b9cf4bf4ff" # "ami-0127d62154efde733"
+  instance_type =  "t2.micro" # "t2.micro"
+  key_name      = "realkeypair"
+  
+  vpc_security_group_ids = [
+   aws_security_group.Nexus-SG.id
+  ]
+
+  tags = {
+    Name = "tomcatserver"
+  }
 }
 
