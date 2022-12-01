ead21184592a58e422cd993e7fe59a2be15bd333
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-12-01 00:02:30 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,13 +2,13 @@ provider "aws" {
    region  = "us-east-1" 
 }
 
-resource "aws_instance" "AWSEC2Instance" {
+resource "aws_instance" "AWSEC2Instance"{
      count   = 1
      ami = "ami-05723c3b9cf4bf4ff"
      instance_type = "t2.micro"
      security_groups = ["Nexus-SG"]
      key_name        = "realkeypair"
-     tags = {
+     tags{
         Name = "tomcatserver"
      }
 }
