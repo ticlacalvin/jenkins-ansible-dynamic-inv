df8132b71f9927cca5f4431930090ef7711d67e7
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-12-04 20:56:36 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -60,7 +60,38 @@ resource "aws_instance" "AWSEC2Instance" {
 tags = {
     Name ="tomcatserver"
   }
-   
+
+resource "aws_key_pair" "realkeypair" {
+ key_name = "realkeypair"
+ public_key = "-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAvfqtvCKlBpH3obpTluhf4OrNdsIk/JbMS1xHdXLqN0SpHLSF
+TRapJXz4oEZQSrmfnMbwQYNWsv77e8NJ50S0tsKnz/f2qXtiJhNiZLcEvkG9H0Mr
+WXL0PxF6hA6kQNHkI681g66VLV4oG3u+6rfZ7C95eZlxrZYUPTzLbsJ2bRv3vzm7
+pgcoWz3hn9qH3tUh/Tr3TROAcsZSiACRGYKK+/yICfoLZ5zTj4HHqGuMKHLeHb9Z
+JhkvImdvp27ErjtLHeILN/zp0PtIEZDorTCFMYx954kltIh5xE3UOmkm7DZIKeB8
+srtGzsR8hVkJGVKwdqDDCieRhSMn6q+k0iPZNwIDAQABAoIBAAluxu2Ocs8utDLt
+g3T8uLX8MpNw9FEuU5pyTIICYKq52AO62tuHAoL//4BFvXrYWJp6Ljeyg72Dd40f
+ly3KdYaVh6uFzlN/8tGdKMeDZtLjhLZmqWJZmNMA1Sp1Z6ZoRYGYE3okUvfbsvYa
+YNJaqpFVkhx9h+1shDi+KgOn2CV2pfnx2ScO124wEUEdJFYX8wYxkD7GNsblGAZz
+vMpT88MWzfFExIVLc5ZDncJ2Pm2j/59nAfz/JYiLaMucWzeZIghgbKofcql4PYB6
++3PN6cEJs08oumD8EZG/0upF4ocR4bYGyCa3i+jEompgVbEPyj+G0F8zphk+JaOW
+EHxVIWECgYEA+I07Nhbyvc+bLpoPSTTUkXb022TpJ836xOlYo5U5Rto/2HYbwSri
+JKC9Cs8fNRwAIsowmpylKhlTLPV2/7bG4yonAFFVVwcgbBHSEh1u4U4iNxifCK9F
+gZU6LFj5f+ymUm+/rZ0I6pYiY6fn+CyN5wzaNuxHX86vLb7NG3NScG8CgYEAw6wb
+i08yvs0hZ+BQjyCeItgMA6YzqjfsNWKRpPnDrF6VvZNXJbpzLe6sgTWAgiuRoRjR
+gaTmNdYuvxMdyB36BVXguTIr1rBzkJL7a9HkIilRtBhkP9D86E5vpnT2bxDWi9db
+AzSPYWOLqTpfmHI85Sjdr1xXC2hkBBrqPagDd7kCgYB7LYr9qGvOc7fFZaZTsnHy
+UVDR9fWyu1sfAUHjeA21tm3TudzRY45Fdr+ZPsKJkIHku+1Qgnc8IOberhu2aP+y
+Du7bMnOnuKlxxXclcR7FW/KK+ZG/PRWmPa1h0wp7Qm2/BxCoWt3TXGpYlxrYHESL
+EAyJEZnhuk2IzzlY9FMT0wKBgAFipo9F8EYJjcL+g5N/157HiM49zv/VhYabPAy+
+9/Owd77v3B4YQ9h/aBlCF7bUOCO72T/huv+GlK4lpIBWNf7zrLOi90x901OitsT8
+p3DWeg2cXplVEXVTmNbE3TnMFLjIvYw4QwudeM+p6iNaK7Qzpd4n+TXLd2nQBviI
+x/2BAoGADLZOK+yZ11UUkyajTM7qoiSm1sXGXViWdht2iegpttZhxLFsKlt1U3HG
+JjZ7XMwUKKSb2uR4cKOW497G5ysAtLWfZGcBKuNW0AY2Wmf8iEDl2onReS7LCDZ7
+pKkYxMaq8NyZzqA10ljTNQEnWqVw3VyKT7VcCEKMGcHyN8tUrjw=
+-----END RSA PRIVATE KEY-----"
+ //public_key = "${file("realkeypair.pub")}
+ 
  
  /*  
   root_block_device {
